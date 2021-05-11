b309ae8da8f6aa1538caec2bc1ae1bbf3593fc8c
Lucas Santiago <lu.santi.oli@gmail.com>
2021-05-11 18:55:57 -0300

    feat: arquivos bases

---
+++ lib/registro/registrar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Registrar extends StatefulWidget {
+  @override
+  _RegistrarState createState() => _RegistrarState();
+}
+
+class _RegistrarState extends State<Registrar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/telas/editar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Editar extends StatefulWidget {
+  @override
+  _EditarState createState() => _EditarState();
+}
+
+class _EditarState extends State<Editar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
