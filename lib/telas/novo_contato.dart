23e7dc9f0d01062a184833d62c1cc649aaf985e3
Lucas Santiago <lu.santi.oli@gmail.com>
2021-05-19 18:58:51 -0300

    feat: tela de edição feita

--- lib/telas/novo_contato.dart
+++ lib/telas/novo_contato.dart
@@ -1,6 +1,8 @@
 import 'package:agenda_de_contatos/providers/contatos.dart';
+import 'package:agenda_de_contatos/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:via_cep_flutter/via_cep_flutter.dart';
 
 class NovoContato extends StatefulWidget {
   static const routeName = 'telas/novo_contato';
@@ -13,6 +15,8 @@ class _NovoContatoState extends State<NovoContato> {
   final _globalKey = GlobalKey<FormState>();
   var _formValues = {};
 
+  var _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,31 +27,96 @@ class _NovoContatoState extends State<NovoContato> {
         child: Form(
             key: _globalKey,
             child: Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(30.0),
               child: Column(
                 children: [
-                  Text('Entre com os dados do contato'),
+                  Text(
+                    'Entre com os dados do contato',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 25.0,
+                  ),
+                  TextFormField(
+                    autocorrect: true,
+                    keyboardType: TextInputType.name,
+                    validator: (value) =>
+                        value.isEmpty ? 'O nome não pode ser vazio!' : '',
+                    onSaved: (nome) => _formValues['nome'] = nome,
+                    decoration: InputDecoration(
+                        labelText: 'Nome', border: OutlineInputBorder()),
+                  ),
                   SizedBox(
                     height: 2.0,
                   ),
                   TextFormField(
-                    autocorrect: true,
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'O nome não pode ser vazio!';
-                      }
-                      return '';
-                    },
-                    onSaved: (nome) => _formValues['nome'] = nome,
+                    autocorrect: false,
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) => !isValidEmail(value)
+                        ? 'É necessário inserir um email válido'
+                        : '',
+                    onSaved: (email) => _formValues['email'] = email,
                     decoration: InputDecoration(
                         labelText: 'Email', border: OutlineInputBorder()),
                   ),
                   SizedBox(
                     height: 2.0,
                   ),
+                  TextFormField(
+                    autocorrect: false,
+                    keyboardType: TextInputType.phone,
+                    validator: (value) => value.isEmpty
+                        ? 'O número de telefone não pode ser vazio!'
+                        : '',
+                    onSaved: (telefone) => _formValues['telefone'] = telefone,
+                    decoration: InputDecoration(
+                        labelText: 'Telefone', border: OutlineInputBorder()),
+                  ),
+                  SizedBox(
+                    height: 2.0,
+                  ),
+                  TextFormField(
+                    autocorrect: false,
+                    keyboardType: TextInputType.number,
+                    maxLength: 8,
+                    validator: (value) =>
+                        value.isEmpty ? 'O CEP não pode estar vazio!' : '',
+                    onSaved: (cep) => _formValues['cep'] = cep,
+                    decoration: InputDecoration(
+                        labelText: 'CEP', border: OutlineInputBorder()),
+                  ),
+                  SizedBox(
+                    height: 2.0,
+                  ),
+                  TextFormField(
+                    autocorrect: true,
+                    validator: (value) =>
+                        value.isEmpty ? 'O endereço não pode ser vazio!' : '',
+                    onSaved: (endereco) => _formValues['endereco'] = endereco,
+                    decoration: InputDecoration(
+                        labelText: 'Endereço', border: OutlineInputBorder()),
+                  ),
+                  // ElevatedButton(
+                  //     onPressed: () {
+                  //       print(getNomeRua('05653-070'));
+                  //     },
+                  //     child: Text('Verificar CEP')),
+                  // SizedBox(
+                  //   height: 2.0,
+                  // ),
+                  // TextFormField(
+                  //   controller: _controller,
+                  //   readOnly: true,
+                  //   decoration: InputDecoration(
+                  //       labelText: 'Rua', border: OutlineInputBorder()),
+                  // ),
+                  // SizedBox(
+                  //   height: 40,
+                  // ),
                   ElevatedButton(
-                      onPressed: () => addContato(),
-                      child: Text('Salvar Contato'))
+                    onPressed: () => addContato(),
+                    child: Text('Salvar Contato'),
+                  ),
                 ],
               ),
             )),
@@ -55,8 +124,24 @@ class _NovoContatoState extends State<NovoContato> {
     );
   }
 
+  Future<String> getNomeRua(String cep) async {
+    if (cep == null || cep.length < 8) return null;
+    final result = await readAddressByCep(cep);
+    return result.street;
+  }
+
   addContato() {
-    Provider.of<Contatos>(context, listen: false)
-        .inserir('nome', 'email', 'endereco', 'cep', 'telefone');
+    _globalKey.currentState.save();
+
+    if (_globalKey.currentState.validate()) {
+      return '';
+    }
+
+    Provider.of<Contatos>(context, listen: false).inserir(
+        _formValues['nome'],
+        _formValues['email'],
+        _formValues['endereco'],
+        _formValues['cep'],
+        _formValues['telefone']);
   }
 }
---
+++ lib/utils/utils.dart
@@ -0,0 +1,6 @@
+/// Regex simples de validação de email
+bool isValidEmail(String email) {
+  return RegExp(
+          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+      .hasMatch(email);
+}
