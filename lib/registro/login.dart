5aed0649ed0f9d2a6c5266c849f1c265c2310bb7
Lucas Santiago <lu.santi.oli@gmail.com>
2021-05-20 17:13:09 -0300

    feat: Modelo da tela de login e de registro feitos

--- lib/registro/login.dart
+++ lib/registro/login.dart
@@ -1,3 +1,4 @@
+import 'package:agenda_de_contatos/utils/utils.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -6,8 +7,65 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final _globalKey = GlobalKey<FormState>();
+  var _formValues = {};
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Entrar na conta'),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _globalKey,
+          child: Column(
+            children: [
+              Text(
+                'Insira os dados de login',
+                style: TextStyle(fontSize: 26),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                autocorrect: true,
+                keyboardType: TextInputType.emailAddress,
+                onSaved: (newValue) => _formValues['email'] = newValue,
+                validator: (value) => (value.isEmpty || !isValidEmail(value))
+                    ? 'Insira um email válido!'
+                    : '',
+                decoration: InputDecoration(
+                    labelText: 'Endereço', border: OutlineInputBorder()),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              TextFormField(
+                autocorrect: false,
+                obscureText: true,
+                onSaved: (newValue) => _formValues['senha'] = newValue,
+                validator: (value) => (value.length < 8)
+                    ? 'Insira uma senha de no mínimo 8 digitos'
+                    : '',
+                decoration: InputDecoration(
+                    labelText: 'Endereço', border: OutlineInputBorder()),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              ElevatedButton(
+                  onPressed: () => entrar(), child: Text('Entrar na conta')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  entrar() {
+    _globalKey.currentState.save();
+
+    if (!_globalKey.currentState.validate()) return;
   }
 }
--- lib/registro/registrar.dart
+++ lib/registro/registrar.dart
@@ -1,13 +1,103 @@
+import 'package:agenda_de_contatos/utils/utils.dart';
 import 'package:flutter/material.dart';
 
 class Registrar extends StatefulWidget {
+  static const routeName = 'registro/registrar';
+
   @override
   _RegistrarState createState() => _RegistrarState();
 }
 
 class _RegistrarState extends State<Registrar> {
+  final _globalKey = GlobalKey<FormState>();
+  var _formValues = {};
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registrar conta'),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+            key: _globalKey,
+            child: Column(
+              children: [
+                Text(
+                  'Insira os dados para se registrar',
+                  style: TextStyle(fontSize: 26),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  autocorrect: true,
+                  keyboardType: TextInputType.name,
+                  initialValue: _formValues['nome'],
+                  onSaved: (value) => _formValues['nome'] = value,
+                  validator: (value) => (value.isEmpty || value.length < 4)
+                      ? 'O nome precisa ter pelo menos 4 caracteres!'
+                      : '',
+                  decoration: InputDecoration(
+                      labelText: 'Nome', border: OutlineInputBorder()),
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                TextFormField(
+                  autocorrect: false,
+                  keyboardType: TextInputType.emailAddress,
+                  initialValue: _formValues['email'],
+                  onSaved: (newValue) => _formValues['email'] = newValue,
+                  validator: (value) => (value.isEmpty || !isValidEmail(value))
+                      ? 'É necessário um email válido!'
+                      : '',
+                  decoration: InputDecoration(
+                      labelText: 'Email', border: OutlineInputBorder()),
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                TextFormField(
+                  autocorrect: false,
+                  initialValue: _formValues['senha'],
+                  obscureText: false,
+                  onSaved: (newValue) => _formValues['senha'] = newValue,
+                  validator: (value) => value.length < 8
+                      ? 'A senha precisa ter no mínimo 8 caracteres'
+                      : '',
+                  decoration: InputDecoration(
+                      labelText: 'Endereço', border: OutlineInputBorder()),
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                TextFormField(
+                  autocorrect: false,
+                  initialValue: _formValues['confirmar_senha'],
+                  obscureText: false,
+                  onSaved: (newValue) =>
+                      _formValues['confirmar_senha'] = newValue,
+                  validator: (value) => value.length < 8
+                      ? 'A senha precisa ter no mínimo 8 caracteres'
+                      : '',
+                  decoration: InputDecoration(
+                      labelText: 'Endereço', border: OutlineInputBorder()),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                ElevatedButton(
+                    onPressed: () => registrar(), child: Text('Registrar')),
+              ],
+            )),
+      ),
+    );
+  }
+
+  registrar() {
+    _globalKey.currentState.save();
+
+    if (!_globalKey.currentState.validate()) return;
   }
 }
